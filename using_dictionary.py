9523b0da4a003de450b1851ba70169674d420669
Hacı Celal Aygar <celal.aygar@gmail.com>
2018-12-24 22:24:59 +0300

    Update using_dictionary.py

--- using_dictionary.py
+++ using_dictionary.py
@@ -18,14 +18,12 @@ print(dic.keys())                   # dict_keys(['turkey', 'germany', 'spaing', 
 
 
 # ------------------------------------------------------------------------------
-
 dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
 print(dict1.items())                        # dict_items([('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5)])
 
 # Double each value in the dictionary
 double_dict1 = {k:v*2 for (k,v) in dict1.items()}
 print(double_dict1)                         # {'a': 2, 'b': 4, 'c': 6, 'd': 8, 'e': 10}
-
 
 double_dict1 = {k:v*v for (k,v) in dict1.items()}
 print(double_dict1)                         # {'a': 1, 'b': 4, 'c': 9, 'd': 16, 'e': 25}
