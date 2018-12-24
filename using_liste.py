2757dc55da5c133b5b6475ed193b303f009652d5
Hacı Celal Aygar <celal.aygar@gmail.com>
2018-12-24 20:52:51 +0300

    Update using_liste.py

--- using_liste.py
+++ using_liste.py
@@ -24,5 +24,15 @@ print("liste3 sort : ",liste3)
 
 print("liste3 count : ",liste3.count(4))
 
+liste1= ["a", "b", "c", "d", "e", "f", "g"]
+liste2=list()
+liste3=list()
+for index, eleman in enumerate(liste1):
+    if (index % 2 == 1):
+        liste2.insert(index,eleman)
+    else:
+        liste3.insert(index,eleman)
 
+print(liste2)       # ['b', 'd', 'f']
+print(liste3)       # ['a', 'c', 'e', 'g']
 
