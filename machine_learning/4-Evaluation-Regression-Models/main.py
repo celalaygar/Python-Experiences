0b0e17ca02f26b54fe3f210423253f0b49334219
Hacı Celal Aygar <celal.aygar@gmail.com>
2019-10-23 23:27:10 +0300

    Update main.py

--- machine_learning/4-Evaluation-Regression-Models/main.py
+++ machine_learning/4-Evaluation-Regression-Models/main.py
@@ -6,14 +6,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 df = pd.read_csv("random-forest-regression-dataset.csv",sep = ";",header = None)
-
 x = df.iloc[:,0].values.reshape(-1,1)
 y = df.iloc[:,1].values.reshape(-1,1)
  
 from sklearn.ensemble import RandomForestRegressor
+
 rf = RandomForestRegressor(n_estimators = 100, random_state = 42)
 rf.fit(x,y)
-
 y_head = rf.predict(x)
 
 print(x)                                # x values
