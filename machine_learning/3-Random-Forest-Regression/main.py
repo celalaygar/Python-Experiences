57951d7de3fc2ded6f833488595d5990c69458b1
Hacı Celal Aygar <celal.aygar@gmail.com>
2019-10-23 23:27:58 +0300

    Update main.py

--- machine_learning/3-Random-Forest-Regression/main.py
+++ machine_learning/3-Random-Forest-Regression/main.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  8 14:44:19 2019
-
-@author: aygar
-"""
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,12 +9,10 @@ df = pd.read_csv("random-forest-regression-dataset.csv",sep = ";",header = None)
 x = df.iloc[:,0].values.reshape(-1,1)   # tüm satırları al ve sadece 1. kolonu al
 y = df.iloc[:,1].values.reshape(-1,1)   # tüm satırları al ve sadece 2. kolonu al
 
-
 from sklearn.ensemble import RandomForestRegressor
+
 rf = RandomForestRegressor(n_estimators = 100, random_state = 42)
-
 rf.fit(x,y)
-
 
 x_ = np.arange(min(x),max(x),0.01).reshape(-1,1)
 y_head = rf.predict(x_)
